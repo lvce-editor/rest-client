304a2fcbdfa3591e71e04dd5c3856b601b2b3f72
Le Vivilet <levivlet@pm.me>
2024-09-24 23:11:11 +0200

    feature: add submit form

--- packages/extension/src/parts/Main/Main.ts
+++ packages/extension/src/parts/Main/Main.ts
@@ -5,7 +5,9 @@ const webViewProvider = {
   },
   async open(uri, webView) {},
   commands: {
-    async handleInput(text) {},
+    async handleSubmit(text) {
+      console.log({ text })
+    },
   },
 }
 
