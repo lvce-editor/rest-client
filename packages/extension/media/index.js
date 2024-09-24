00b1a5467700488d5e29864440d5487293f816ed
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-24 23:15:54 +0200

    feature: add submit form (#2)

--- packages/extension/media/index.js
+++ packages/extension/media/index.js
@@ -1,13 +1,48 @@
 // TODO use virtual dom in  worker
 
-const initialize = (parsedNodes, timings) => {
+const handleSubmit = async () => {
+  const input = document.getElementById('Input')
+  if (!input) {
+    throw new Error('input not found')
+  }
+  // @ts-ignore
+  await rpc.invoke('handleSubmit', input.value)
+}
+
+const initialize = (inputValue) => {
   const app = document.createElement('div')
   app.className = 'App'
-  app.textContent = 'Hello Rest Client'
+
+  const heading = document.createElement('h1')
+  heading.textContent = 'Rest Client'
+
+  const input = document.createElement('input')
+  input.value = inputValue
+  input.id = 'Input'
+
+  const button = document.createElement('button')
+  button.textContent = 'run'
+
+  const form = document.createElement('form')
+  form.append(input, button)
+
+  const output = document.createElement('output')
+  output.id = 'Output'
+
+  app.append(heading, form, output)
 
   document.body.append(app)
 }
 
+const setOutput = (value) => {
+  const output = document.getElementById('Output')
+  if (!output) {
+    throw new Error('output not found')
+  }
+  output.textContent = value
+}
+
 const rpc = globalThis.lvceRpc({
   initialize,
+  setOutput,
 })
