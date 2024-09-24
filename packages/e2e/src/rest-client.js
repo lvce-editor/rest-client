03a504dbdafdd1bcce337a11f03f1bc72b3e57be
Le Vivilet <levivlet@pm.me>
2024-09-24 22:50:38 +0200

    rename

--- packages/e2e/src/rest-client.js
+++ packages/e2e/src/rest-client.js
@@ -1,7 +1,7 @@
-export const name = 'heap-snapshot-viewer'
+export const name = 'rest-client'
 
 export const skip = true
 
 export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
-  // TODO verify heapsnapshot can be opened
+  // TODO verify request can be executed
 }
