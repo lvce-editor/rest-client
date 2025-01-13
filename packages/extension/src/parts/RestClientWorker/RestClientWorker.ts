57902994d6b9b81275a7177d980dbc7c56bb5805
Le Vivilet <levivlet@pm.me>
2025-01-13 22:32:22 +0100

    remove unused code

--- packages/extension/src/parts/RestClientWorker/RestClientWorker.ts
+++
@@ -1,6 +0,0 @@
-import * as GetOrCreateWorker from '../GetOrCreateWorker/GetOrCreateWorker.ts'
-import * as LaunchRestClientWorker from '../LaunchRestClientWorker/LaunchRestClientWorker.ts'
-
-const { invoke } = GetOrCreateWorker.getOrCreateWorker(LaunchRestClientWorker.launchRestClientWorker)
-
-export { invoke }
--- packages/extension/src/parts/RestClientWorkerUrl/RestClientWorkerUrl.ts
+++
@@ -1,3 +0,0 @@
-import * as AssetDir from '../AssetDir/AssetDir.ts'
-
-export const restClientWorkerUrl = `${AssetDir.assetDir}/rest-client-worker/src/restClientWorkerMain.ts`
