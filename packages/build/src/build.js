e2d3431221e6833191fd9700717b261584cb555c
Le Vivilet <levivlet@pm.me>
2025-01-13 22:32:31 +0100

    build

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -38,13 +38,6 @@ await replace({
   replacement: 'dist/restClientWorkerMain.js',
 })
 
-const assetDirPath = path.join(root, 'dist', 'src', 'parts', 'AssetDir', 'AssetDir.ts')
-await replace({
-  path: assetDirPath,
-  occurrence: '../../../../',
-  replacement: '../',
-})
-
 await replace({
   path: join(root, 'dist', 'extension.json'),
   occurrence: 'src/restClientMain.ts',
