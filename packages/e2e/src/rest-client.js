b1acacb3e491cab8d5d4fade38863890eff0bb7d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-13 22:14:49 +0100

    feature: add e2e test (#13)

--- packages/e2e/src/rest-client.js
+++ packages/e2e/src/rest-client.js
@@ -1,7 +1,17 @@
 export const name = 'rest-client'
 
-export const skip = true
-
+// TODO maybe use localhost url for test
+// but make the localhost url dynamic so that it also works on gitpod
+// or codespaces
 export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
-  // TODO verify request can be executed
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.rest`, `GET https://example.com`)
+
+  // act
+  await Main.openUri(`${tmpDir}/test.rest`)
+
+  // assert
+  const webView = Locator('.WebViewIframe')
+  await expect(webView).toBeVisible()
 }
