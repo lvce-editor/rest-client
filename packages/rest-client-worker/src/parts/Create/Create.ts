75a89a4bfbf25483b43d405e05dcf4647f63fea8
Le Vivilet <levivlet@pm.me>
2025-01-13 22:28:22 +0100

    fix

--- packages/rest-client-worker/src/parts/Create/Create.ts
+++ packages/rest-client-worker/src/parts/Create/Create.ts
@@ -1,9 +1,11 @@
 import type { WebView } from '../WebView/WebView.ts'
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
-export const create = (id: number, port: MessagePort): void => {
+export const create = (id: number, port: MessagePort, method: string, url: string): void => {
   const webview: WebView = {
     port,
+    method,
+    url,
   }
   WebViewStates.set(id, webview)
 }
