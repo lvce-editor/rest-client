0294ffbf0bc3194ba93880b45dc49fed8ecd5e33
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-21 23:10:54 +0100

    feature: use new webworker api (#24)
    * update dependencies (#20)
    #20
    
    * Discard changes to packages/extension/extension.json
    
    * use new api
    
    * call main
    
    * fix api
    
    * fix initial code
    
    * fix error
    
    * skip

--- packages/rest-client-worker/src/parts/Create2/Create2.ts
+++ packages/rest-client-worker/src/parts/Create2/Create2.ts
@@ -1,10 +1,26 @@
 import * as Create from '../Create/Create.ts'
 import { id } from '../Id/Id.ts'
-import * as Rpc from '../Rpc/Rpc.ts'
+
+const getInitialCode = (content: string) => {
+  const [method, url] = content.split(' ')
+  if (method && url) {
+    return {
+      method,
+      url,
+    }
+  }
+  return {
+    method: 'GET',
+    url: 'https://example.com',
+  }
+}
 
 export const create2 = async ({ port, savedState, webViewId, uri }) => {
-  const content = await Rpc.invoke('WebView.readFile', uri)
-  const [method, url] = content.split(' ')
+  // TODO avoid global variable
+  // @ts-ignore
+  const rpc = globalThis.rpc
+  const content = await rpc.invoke('WebView.readFile', uri)
+  const { method, url } = getInitialCode(content)
   Create.create(id, port, method, uri)
   await port.invoke('initialize', method, url)
   return {}
